0e5ae39ef4cabb00bb2e150e7a702d7b24304e08
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2022-03-20 14:25:55 -0400

    Here is the correct file

---
+++ Code/CarMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+    public float speed = 3.0f; 
+      
+    void Update()
+
+    {
+
+        // Hint: In Unity You can get Vector (direction) shorthands like this:
+
+        // Vector3.up
+
+        // I searched the internet to help me out
+
+        // Here is the Link:
+        // https://answers.unity.com/questions/609527/how-do-i-make-a-game-object-move-in-the-direction.html
+
+
+
+        float xDirection = Input.GetAxisRaw("Vertical");
+
+        transform.position += transform.right * Time.deltaTime * speed;
+
+
+        
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
