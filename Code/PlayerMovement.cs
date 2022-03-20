7b3bc09aa4e3730c94b9f2e1e693019e2aaff9e5
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2022-03-20 14:22:07 -0400

    Here are the files for HW Assignment

---
+++ Code/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    
+ public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+
+
+        float xMovement = xMove * speed * Time.deltaTime;
+        float yMovement = yMove * speed * Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x + xMovement, 0, 0);
+
+        transform.Translate(xMovement, yMovement, 0);
+
+    }
+}
