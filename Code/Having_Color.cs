1192829b3873c93180f3f2a1602ac0381d711a03
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2022-03-28 16:00:33 -0400

    Update Having_Color.cs

---
+++ Code/Having_Color.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Having_Color : MonoBehaviour
+{
+   public Color myColor;
+ 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        }
+    }
+
+
