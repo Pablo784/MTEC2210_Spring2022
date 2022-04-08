facd1b32405dfc053dc1de80f5be2aad8e954ebb
Pablo784 <77360636+Pablo784@users.noreply.github.com>
2022-04-07 22:19:19 -0400

    AFTER THE CAR HITS FROGGY; SCENE RESETS :arrows_counterclockwise:

--- Code/Killing_Frogger.cs
+++ Code/Killing_Frogger.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Killing_Frogger : MonoBehaviour
 
@@ -17,14 +19,30 @@ public class Killing_Frogger : MonoBehaviour
 
     }
 
-    // HERE WE KILL FROGGY
-    // GOT HELP FROM CLASSMATES AND THE PROFESSOR ESPECIALLY
+    /* 
+     HERE WE KILL FROGGY
+     GOT HELP FROM CLASSMATES AND THE PROFESSOR ESPECIALLY
+    */
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Car")
         {
             Destroy(gameObject);
+
+
         }
 
+        /* 
+         AFTER FROGGY IS KILLED BY THE CAR THE SCENE RESETS
+         I CONSULTED A YOUTUBE VIDEO FOR HELP
+         HERE IS THE LINK:
+         https://youtu.be/UMJ7Az9WaIU
+
+        */
+
+        Debug.Log("The Scene has Reseted");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
